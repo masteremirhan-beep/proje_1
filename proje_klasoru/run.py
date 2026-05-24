afd57f2d27ec911fb27f2303df292291f7f070da
masteremirhan-beep <masteremirhan@gmail.com>
2026-05-24 21:12:26 +0300

    Delete proje_klasoru directory

--- proje_klasoru/run.py
+++
@@ -1,8 +0,0 @@
-# run.py - Uygulamayı başlat
-import tkinter as tk
-from login import LoginScreen
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    login = LoginScreen(root)
-    root.mainloop()
--- proje_klasoru/test_whitebox.py
+++
@@ -1,5 +0,0 @@
-# test_whitebox.py
-import whitebox
-wbt = whitebox.WhiteboxTools()
-print("WhiteboxTools versiyonu:", wbt.version())
-print("✅ Kurulum başarılı!")
